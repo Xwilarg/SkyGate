3ef49984df65cd5f8f8a40c5750cd5f0ddc944da
Xwilarg <xwilarg@protonmail.com>
2022-09-16 12:59:02 +0200

    Fix vertical scrolling

--- Assets/Scripts/Game/GridManager.cs
+++ Assets/Scripts/Game/GridManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace SkyGate.Game
@@ -14,14 +15,14 @@ namespace SkyGate.Game
         private readonly List<Transform> _horizontalLines = new();
 
         private const float _scrollingSpeed = 200f;
+        private const float _padding = 100f;
 
         private void Start()
         {
-            var padding = 100;
-            for (int i = 0; i < (Screen.height / padding) + padding; i++)
+            for (int i = 0; i < (Screen.height / _padding) + _padding; i++)
             {
                 var go = Instantiate(_horizontalLinePrefab, _gridContainer);
-                ((RectTransform)go.transform).anchoredPosition = new Vector2(0f, i * padding);
+                ((RectTransform)go.transform).anchoredPosition = new Vector2(0f, i * _padding);
                 _horizontalLines.Add(go.transform);
             }
         }
@@ -36,7 +37,7 @@ namespace SkyGate.Game
                     rTransform.anchoredPosition = new Vector2(0f, rTransform.anchoredPosition.y - (Time.deltaTime * _scrollingSpeed));
                     if (rTransform.anchoredPosition.y < 0f)
                     {
-                        rTransform.anchoredPosition = new(0f, rTransform.anchoredPosition.y + Screen.height);
+                        rTransform.anchoredPosition = new(0f, _horizontalLines.Max(x => ((RectTransform)x).anchoredPosition.y) + _padding);
                     }
                 }
             }
