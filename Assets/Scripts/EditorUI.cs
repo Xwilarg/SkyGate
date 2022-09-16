125ad83520fa7ec1ea894d9f4e945c4a408c4ecf
Xwilarg <xwilarg@protonmail.com>
2022-09-16 03:02:44 +0200

    Add input file path

---
+++ Assets/Scripts/EditorUI.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using TMPro;
+using UnityEngine;
+
+namespace SkyGate
+{
+    public class EditorUI : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_InputField _filePath;
+
+        public void LoadSong()
+        {
+            var path = _filePath.text;
+            if (File.Exists(path))
+            {
+                Debug.Log(path);
+            }
+            else
+            {
+                Debug.LogWarning($"Failed to load {path}");
+            }
+        }
+    }
+}
